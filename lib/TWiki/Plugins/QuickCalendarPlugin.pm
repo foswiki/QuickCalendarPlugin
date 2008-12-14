7bcf71cfdfa8c7f64c05bed393fed89cbebe19ea
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2008-12-14 17:49:56 +0000

    Item27: Fix svn properties on all .pm, .tmpl and .txt files
    git-svn-id: http://svn.foswiki.org/trunk/QuickCalendarPlugin@1340 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/TWiki/Plugins/QuickCalendarPlugin.pm
+++ lib/TWiki/Plugins/QuickCalendarPlugin.pm
@@ -92,10 +92,10 @@ use vars qw(
         %This %Next %Last %Prev
     );
 
-# This should always be $Rev: 8123 $ so that TWiki can determine the checked-in
+# This should always be $Rev$ so that TWiki can determine the checked-in
 # status of the plugin. It is used by the build automation tools, so
 # you should leave it alone.
-$VERSION = '$Rev: 8123 $';
+$VERSION = '$Rev$';
 
 # This is a free-form string you can use to "name" your own plugin version.
 # It is *not* used by the build automation tools, but is reported as part
